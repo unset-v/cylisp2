e40cd560eadb3ea2db924f997c67a30cbb0cd734
Student <84834654+unset-v@users.noreply.github.com>
2021-12-01 15:40:12 -0500

    re-uploads

---
+++ cylib.scm
@@ -0,0 +1,127 @@
+(begin
+
+  (define else  #t)
+  (define nil   (' ()))
+
+  (define null? (lambda (lst)   (eq? lst nil)))
+  (define apply (lambda (f x)   (f x)))
+
+  (define buf  (lambda (a)   a))
+  (define not  (lambda (a)   (if a #f #t)))
+  (define or   (lambda (a b) (if a #t  b)))
+  (define and  (lambda (a b) (if a  b #f)))
+  (define nor  (lambda (a b) (not (or a b))))
+  (define nand (lambda (a b) (not (and a b))))
+  (define xor  (lambda (a b) (or (and (not a) b) (and a (not b)))))
+
+  (define ++  (lambda (n)   (+ n 1)))
+  (define --  (lambda (n)   (- n 1)))
+  (define neg (lambda (n)   (- 0 n)))
+  (define mod (lambda (x y) (- x (* (// x y) y))))
+
+  (define negative? (lambda (n) (< n 0)))
+  (define zero?     (lambda (n) (= n 0)))
+  (define positive? (lambda (n) (> n 0)))
+  (define even?     (lambda (n) (= (mod n 2) 0)))
+  (define odd?      (lambda (n) (not (even? n))))
+
+  (define abs (lambda (n)   (if (<= n 0) (neg n) n)))
+  (define pow (lambda (b e) (if (= e 1) b (* b (pow b (-- e))))))
+  (define !   (lambda (n)   (if (= n 1) 1 (* n (! (-- n))))))
+  
+  (define caar   (lambda (x) (car (car x))))
+  (define cadr   (lambda (x) (car (cdr x))))
+  (define cdar   (lambda (x) (cdr (car x))))
+  (define cddr   (lambda (x) (cdr (cdr x))))
+  (define caaar  (lambda (x) (car (car (car x)))))
+  (define caadr  (lambda (x) (car (car (cdr x)))))
+  (define cadar  (lambda (x) (car (cdr (car x)))))
+  (define caddr  (lambda (x) (car (cdr (cdr x)))))
+  (define cdaar  (lambda (x) (cdr (car (car x)))))
+  (define cdadr  (lambda (x) (cdr (car (cdr x)))))
+  (define cddar  (lambda (x) (cdr (cdr (car x)))))
+  (define cdddr  (lambda (x) (cdr (cdr (cdr x)))))
+  (define caaaar (lambda (x) (car (car (car (car x))))))
+  (define caaadr (lambda (x) (car (car (car (cdr x))))))
+  (define caadar (lambda (x) (car (car (cdr (car x))))))
+  (define caaddr (lambda (x) (car (car (cdr (cdr x))))))
+  (define cadaar (lambda (x) (car (cdr (car (car x))))))
+  (define cadadr (lambda (x) (car (cdr (car (cdr x))))))
+  (define caddar (lambda (x) (car (cdr (cdr (car x))))))
+  (define cadddr (lambda (x) (car (cdr (cdr (cdr x))))))
+  (define cdaaar (lambda (x) (cdr (car (car (car x))))))
+  (define cdaadr (lambda (x) (cdr (car (car (cdr x))))))
+  (define cdadar (lambda (x) (cdr (car (cdr (car x))))))
+  (define cdaddr (lambda (x) (cdr (car (cdr (cdr x))))))
+  (define cddaar (lambda (x) (cdr (cdr (car (car x))))))
+  (define cddadr (lambda (x) (cdr (cdr (car (cdr x))))))
+  (define cdddar (lambda (x) (cdr (cdr (cdr (car x))))))
+  (define cddddr (lambda (x) (cdr (cdr (cdr (cdr x))))))
+
+  (define list 
+    (lambda (x y) (cons x (cons y (' ())))))
+  
+  (define append
+    (lambda (x y)
+      (if (null? x) y
+        (cons (car x) (append (cdr x) y)))))
+
+  (define pair
+    (lambda (x y)
+      (cond ((and (null? x) (null? y)) (' ()))
+            ((and (not (atom? x)) (not (atom? y)))
+             (cons (list (car x) (car y))
+                   (pair (cdr x) (cdr y)))))))
+
+  (define assoc
+    (lambda (x y)
+      (if (eq? (caar y) x) (cadar y)
+        (assoc x (cdr y)))))
+
+  (define map
+    (lambda (fn lst)
+      (if (null? lst) lst
+        (cons (fn (car lst)) (map fn (cdr lst))))))
+
+  (define range
+    (lambda (a b)
+      (if (= a b) (quote ()) (cons a (range (++ a) b)))))
+  
+  (define eval 
+    (lambda (e a)
+      (cond
+        ((atom? e) (assoc e a))
+        ((atom? (car e))
+         (cond
+           ((eq? (car e) (' quote)) (cadr e))
+           ((eq? (car e) (' atom?)) (atom?  (eval (cadr e) a)))
+           ((eq? (car e) (' eq?))   (eq?    (eval (cadr e) a)
+                                            (eval (caddr e) a)))
+           ((eq? (car e) (' car))   (car    (eval (cadr e) a)))
+           ((eq? (car e) (' cdr))   (cdr    (eval (cadr e) a)))
+           ((eq? (car e) (' cons))  (cons   (eval (cadr e) a)
+                                            (eval (caddr e) a)))
+           ((eq? (car e) (' cond))  (evcon  (cdr e) a))
+           (else (eval (cons (assoc (car e) a)
+                              (cdr e))
+                       a))))
+        ((eq? (caar e) (' label))
+         (eval (cons (caddar e) (cdr e))
+               (cons (list (cadar e) (car e)) a)))
+        ((eq? (caar e) (' lambda))
+         (eval (caddar e)
+               (append (pair (cadar e) (evlis (cdr e) a))
+                       a))))))
+  
+    (define evcon 
+      (lambda (c a)
+        (cond ((eval (caar c) a)
+               (eval (cadar c) a))
+              (else (evcon (cdr c) a)))))
+
+    (define evlis 
+      (lambda (m a)
+        (cond ((null? m) (' ()))
+              (else (cons (eval  (car m) a)
+                          (evlis (cdr m) a))))))
+)
---
+++ cylisp.py
@@ -0,0 +1,144 @@
+import operator as op
+import sys
+
+def tokenize(prog):
+    return prog.replace('(', " ( ").replace(')', " ) ").split()
+
+def atomic(token):
+    # TODO: too ungly, re-write required
+
+    try: return int(token)
+    except ValueError:
+        try: return float(token)
+        except ValueError: return str(token)
+
+def parse(tokens):
+    tree = []
+    while tokens:
+        token = tokens.pop(0)
+        if   token == ')' : break
+        elif token == '(' : tree += [parse(tokens)]
+        else              : tree += [atomic(token)]
+
+    return tree
+
+def default_env():
+
+    def _atom(x):
+        return type(x) != list or x == []
+
+    return {
+        # boolean values
+        '#t' : True, '#f' : False,
+
+        # arithmetic operators
+        '+'  : op.add     , '-'  : op.sub, '*' : op.mul,
+        '/'  : op.truediv , '>'  : op.gt , '<' : op.lt ,
+        '>=' : op.ge      , '<=' : op.le , '=' : op.eq ,
+        '//' : op.floordiv,
+
+        # LISP primitive operators
+        'eq?'     : lambda x, y: x == y or [],
+        'car'     : lambda x: x[0]  if not _atom(x) else [],
+        'cdr'     : lambda x: x[1:] if not _atom(x) else [],
+        'cons'    : lambda x, y: [x] + list(y),
+        'atom?'   : lambda x: _atom(x) or [],
+
+        # other primitives
+        'begin'   : lambda *x: x[-1],
+        'display' : lambda  s: print(prettify(s), end = '') or '',
+        'newline' : lambda   : sys.stdout.write('\n')
+    }
+
+def evaluate(tree, env):
+
+    # symbols and constant literals
+    if       isinstance(tree, str)  : return env[tree]
+    elif not isinstance(tree, list) : return tree
+
+    # special cases
+    if tree[0] in ("quote", '"', "'"):
+
+        if   tree[1:][0]   == [] : return []
+        elif len(tree[1:]) == 1  : return tree[1:][0]
+        else                     : return tree[1:]
+
+    elif tree[0] == 'define':
+        (symbol, expression) = tree[1:]
+        env[symbol] = evaluate(expression, env)
+
+        return symbol
+
+    elif tree[0] == 'if':
+        (predi, conseq, alter) = tree[1:]
+        expression = (conseq if evaluate(predi, env) else alter)
+
+        return evaluate(expression, env)
+
+    elif tree[0] == 'cond':
+        clauses = tree[1:]
+        for clause in clauses:
+            if evaluate(clause[0], env):
+
+                return evaluate(clause[1], env)
+
+        return
+
+    elif tree[0] == 'lambda':
+        (formal_param, body) = tree[1:]
+
+        def _procedure(*_args):
+            _env = dict(env)
+            _env.update(zip(formal_param, _args))
+
+            return evaluate(body, _env)
+
+        return _procedure
+
+    # general applicative-order evaluation
+    operands = []
+    operator = evaluate(tree[0], env)
+    for operand in tree[1:]:
+        operands += [evaluate(operand, env)]
+
+    return operator(*operands)
+
+def prettify(expression):
+    if isinstance(expression, list):
+        tokens = []
+        for token in expression:
+            tokens += [prettify(token)]
+
+        return '(' + ' '.join(tokens) + ')'
+
+    elif isinstance(expression, bool):
+
+        return '#t' if expression else '#f'
+
+    return str(expression)
+
+def repl():
+    print("Welcome to CyLISP v0.1-alpha.")
+
+    env = default_env()
+
+    # load library
+    with open("cylib.scm", 'r') as f:
+         content = f.readlines()
+    library = ''.join([x.strip() for x in content])
+    evaluate(parse(tokenize(library))[0], env)
+
+    while True:
+        try:
+            program = input("> ")
+        except (EOFError, KeyboardInterrupt):
+            print("\nDieu vous comant.")
+
+            return
+
+        if program:
+            print(prettify(evaluate(parse(tokenize(program))[0], env)))
+
+    return
+
+__name__ == "__main__" and repl()
